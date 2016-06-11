11217f8c2ae62e2d3e21e46b78e8836474d1c4c3
Mandy Jor <mandy1jor@gmail.com>
2016-06-10 23:17:57 -0500

    Finished factorial exercise (Forgot to commit in between tests..! Still getting used to TDD ;) )

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,18 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        int result;
+        if (i == 0 || i == 1){
+            result = 1;
+        }else if(i > 1){
+            result = 1;
+            while (i > 1){
+                result = result *i;
+                i--;
+            }
+        }else
+            throw new IllegalArgumentException("Cannot take factorial of negative number.");
+
+        return result;
     }
 }
